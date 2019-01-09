f08466cb27d3ad13a48f20a7d906125ba596983a
EvgeniyAbashin <abashins@yandex.ru>
2019-01-09 22:38:16 +0300

    update classes

---
+++ notepad.rb
@@ -0,0 +1,34 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative "post.rb"
+require_relative "link.rb"
+require_relative "memo.rb"
+require_relative "task.rb"
+
+puts "Что вы хотите записать в блокнот?"
+
+choises = Post.post_types
+
+choice = -1
+
+until choice >= 0 && choice < choises.size
+
+  choises.each_with_index { |type, index| puts "\t#{index}. #{type}" }
+
+  choice = STDIN.gets.to_i
+end
+
+entry = Post.create(choice)
+
+entry.read_from_console
+
+entry.save
+
+puts "Запись сохранена."
+
